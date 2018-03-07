ddf2f9f45e3111ee82d787c412921817d87fa687
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2018-03-07 11:24:30 +0300

    feat: Add movie contract with movie entry
    Add contract class for the movies database.
    Add entry class for the movie table.

---
+++ app/src/main/java/io/github/ilya_lebedev/popularmovies/data/MoviesContract.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2018 Ilya Lebedev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.ilya_lebedev.popularmovies.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Defines table and column names for the movies database.
+ */
+public class MoviesContract {
+
+    /* This inner class defines the table content of the movie table */
+    public static final class MovieEntry implements BaseColumns {
+
+        /* Name of movie table */
+        public static final String TABLE_NAME = "movie";
+
+        /* Movie ID as returned by TMDb API, used to identify concrete movie */
+        public static final String COLUMN_MOVIE_ID = "movie_id";
+
+        /* Title is stored as String representing movie title */
+        public static final String COLUMN_TITLE = "title";
+
+        /* Vote average is stored as a float representing movie user rating */
+        public static final String COLUMN_VOTE_AVERAGE = "vote_average";
+
+        /* Popularity is stored as a float representing movie popularity */
+        public static final String COLUMN_POPULARITY = "popularity";
+
+        /* Poster path is stored as String representing relative path to a move poster image */
+        public static final String COLUMN_POSTER_PATH = "poster_path";
+
+        /* Overview is stored as String representing movie overview */
+        public static final String COLUMN_OVERVIEW = "overview";
+
+        /* Release date is stored as long representing movie release date (in milliseconds) */
+        public static final String COLUMN_RELEASE_DATE = "release_date";
+
+    }
+
+}
