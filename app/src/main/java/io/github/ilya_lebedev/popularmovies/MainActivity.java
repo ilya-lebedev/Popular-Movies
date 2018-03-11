a03a382a13f4b3e359f69ed18b887d3c510d8de8
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2018-03-11 20:52:57 +0300

    feat: Add projection, indices and copyright to the MainActivity

--- app/src/main/java/io/github/ilya_lebedev/popularmovies/MainActivity.java
+++ app/src/main/java/io/github/ilya_lebedev/popularmovies/MainActivity.java
@@ -1,13 +1,58 @@
+/*
+ * Copyright (C) 2018 Ilya Lebedev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.ilya_lebedev.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.github.ilya_lebedev.popularmovies.data.MoviesContract;
+
+/**
+ * Main activity of the app.
+ */
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    /*
+     * The columns which we need for displaying list of movies within MainActivity.
+     */
+    public static final String[] MAIN_MOVIE_PROJECTION = {
+            MoviesContract.MovieEntry._ID,
+            MoviesContract.MovieEntry.COLUMN_MOVIE_ID,
+            MoviesContract.MovieEntry.COLUMN_TITLE,
+            MoviesContract.MovieEntry.COLUMN_POSTER_PATH
+
+    };
+
+    /*
+     * This indices representing the values in the array of String above.
+     * Uses for more quickly access to the data from query.
+     * WARN: If the order or the contents of the Strings above changes,
+     * these indices must be adjust to match the changes.
+     */
+    public static final int INDEX_MOVIE_LOCAL_ID = 0;
+    public static final int INDEX_MOVIE_THMBD_ID = 1;
+    public static final int INDEX_MOVIE_TITLE = 2;
+    public static final int INDEX_MOVIE_POSTER_PATH = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
