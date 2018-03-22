5c2a4a409a9b76043944632ba68bb1b3d929b5bb
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2018-03-22 16:28:52 +0300

    Implement movie detail activity

--- app/src/main/java/io/github/ilya_lebedev/popularmovies/MovieDetailActivity.java
+++ app/src/main/java/io/github/ilya_lebedev/popularmovies/MovieDetailActivity.java
@@ -15,14 +15,137 @@
  */
 package io.github.ilya_lebedev.popularmovies;
 
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MovieDetailActivity extends AppCompatActivity {
+import com.squareup.picasso.Picasso;
+
+import io.github.ilya_lebedev.popularmovies.data.MoviesContract;
+import io.github.ilya_lebedev.popularmovies.utilities.NetworkUtils;
+import io.github.ilya_lebedev.popularmovies.utilities.TmdbDateUtils;
+
+/**
+ * MovieDetailActivity
+ */
+public class MovieDetailActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final String TAG = MovieDetailActivity.class.getSimpleName();
+
+    public static final String[] DETAIL_MOVIE_PROJECTION = {
+            MoviesContract.MovieEntry.COLUMN_MOVIE_ID,
+            MoviesContract.MovieEntry.COLUMN_TITLE,
+            MoviesContract.MovieEntry.COLUMN_POSTER_PATH,
+            MoviesContract.MovieEntry.COLUMN_RELEASE_DATE,
+            MoviesContract.MovieEntry.COLUMN_VOTE_AVERAGE,
+            MoviesContract.MovieEntry.COLUMN_POPULARITY,
+            MoviesContract.MovieEntry.COLUMN_OVERVIEW
+    };
+
+    /*
+     * Indexes.
+     */
+    public static final int INDEX_MOVIE_TMDB_ID = 0;
+    public static final int INDEX_MOVIE_TITLE = 1;
+    public static final int INDEX_MOVIE_POSTER_PATH = 2;
+    public static final int INDEX_MOVIE_RELEASE_DATE = 3;
+    public static final int INDEX_MOVIE_VOTE_AVERAGE = 4;
+    public static final int INDEX_MOVIE_POPULARITY = 5;
+    public static final int INDEX_MOVIE_OVERVIEW = 6;
+
+    private static final int ID_DETAIL_MOVIE_LOADER = 17;
+
+    private TextView mTitleTv;
+    private ImageView mPosterIv;
+    private TextView mReleaseDateTv;
+    private TextView mRatingTv;
+    private TextView mPopularityTv;
+    private TextView mOverviewTv;
+
+    private Uri mUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
+
+        mTitleTv = findViewById(R.id.tv_movie_title);
+        mPosterIv = findViewById(R.id.iv_movie_poster);
+        mReleaseDateTv = findViewById(R.id.tv_movie_release_date);
+        mRatingTv = findViewById(R.id.tv_movie_rating);
+        mPopularityTv = findViewById(R.id.tv_movie_popularity);
+        mOverviewTv = findViewById(R.id.tv_movie_overview);
+
+        mUri = getIntent().getData();
+        if (mUri == null) {
+            throw new NullPointerException("URI for MovieDetailActivity cannot be null");
+        }
+
+        getSupportLoaderManager().initLoader(ID_DETAIL_MOVIE_LOADER, null, this);
     }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int loaderId, Bundle args) {
+
+        switch (loaderId) {
+
+            case ID_DETAIL_MOVIE_LOADER: {
+                return new CursorLoader(
+                        this,
+                        mUri,
+                        DETAIL_MOVIE_PROJECTION,
+                        null,
+                        null,
+                        null);
+            }
+
+            default:
+                throw new RuntimeException("Loader Not Implemented: " + loaderId);
+        }
+
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+        boolean cursorHasValidData = false;
+        if (data != null && data.moveToFirst()) {
+            cursorHasValidData = true;
+        }
+
+        if (!cursorHasValidData) {
+            return;
+        }
+
+        String movieTitle = data.getString(INDEX_MOVIE_TITLE);
+        String moviePosterPath = data.getString(INDEX_MOVIE_POSTER_PATH);
+        String moviePosterUrl = NetworkUtils.getMoviePosterUrl(this, moviePosterPath);
+        long movieReleaseDateMillis = data.getLong(INDEX_MOVIE_RELEASE_DATE);
+        String movieReleaseDate = TmdbDateUtils
+                .getFriendlyReleaseDateString(this, movieReleaseDateMillis);
+        double movieVoteAverage = data.getDouble(INDEX_MOVIE_VOTE_AVERAGE);
+        String movieRating = getString(R.string.format_rating, movieVoteAverage);
+        double moviePopularity = data.getDouble(INDEX_MOVIE_POPULARITY);
+        String moviePopularityString = getString(R.string.format_popularity, moviePopularity);
+        String movieOverview = data.getString(INDEX_MOVIE_OVERVIEW);
+
+        mTitleTv.setText(movieTitle);
+        Picasso.with(this).load(moviePosterUrl).into(mPosterIv);
+        mReleaseDateTv.setText(movieReleaseDate);
+        mRatingTv.setText(movieRating);
+        mPopularityTv.setText(moviePopularityString);
+        mOverviewTv.setText(movieOverview);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+    }
+
 }
